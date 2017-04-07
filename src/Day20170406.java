0f6c6eb12d3c1a26d2ff599877d065ed4548d2e9
ITSchool.user <ITSchool.user>
2017-04-07 16:42:23 +0400

    xndir

--- src/Day20170406.java
+++ src/Day20170406.java
@@ -5,29 +5,23 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class Day20170406 {
     public static void main(String[] args) {
-        //avogadroyi tiv@ 1-ic 1001 mijakayqum
-        int a , b;
-        int count ;
-         for(int n = 1; n < 1001; ++n){
-          a = n;
-          count = 0;
-          while( a != 0){
-             a /=10;
-             ++count;
-          }
+        int n = 1191;
+        boolean status = false;
+        int a;
+        a = n;
+        while(a != 0) {
+            a = a / 10;
+            System.out.println("a = " + a);
+            double b = Math.sqrt(a);
+            System.out.println(b);
+            int c = (int) b;
 
-          a = n;
-          int sum = 0;
-          while( a != 0 ){
-              b = a % 10;
-              sum += (int) Math.pow(b, count);
-              a /= 10;
-          }
-
-          if(sum == n){
-              System.out.println(n);
-          }
-
-       }
+            System.out.println(c);
+            if(b == c && c>0 ){
+                status = true;
+                System.out.println("yes");
+                break;
+            }
+        }
     }
 }

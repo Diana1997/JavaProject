9240ca43cfbc5d6aa255e7a62e7b16437be3f0a4
ITSchool.user <ITSchool.user>
2017-04-07 16:18:01 +0400

    avogadroyiTiv

--- src/Day20170406.java
+++ src/Day20170406.java
@@ -5,37 +5,29 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class Day20170406 {
     public static void main(String[] args) {
-     //   for(int ){}
-    //    int n = 10;
+        //avogadroyi tiv@ 1-ic 1001 mijakayqum
+        int a , b;
+        int count ;
+         for(int n = 1; n < 1001; ++n){
+          a = n;
+          count = 0;
+          while( a != 0){
+             a /=10;
+             ++count;
+          }
 
-        for(double i = Math.PI / 16; i<Math.PI / 2; i += Math.PI / 16){
-            System.out.println(1 / Math.tan(i));
-        }
+          a = n;
+          int sum = 0;
+          while( a != 0 ){
+              b = a % 10;
+              sum += (int) Math.pow(b, count);
+              a /= 10;
+          }
 
-      /*  int n = 5,  f=1;
-        for(int i = 1; i<n+1; ++i){
-            f *= i;
-        }
-        System.out.println(f);
+          if(sum == n){
+              System.out.println(n);
+          }
 
-        /*int n = 2;
-        int f = 1;
-        double sum = 1;
-        for (int i = 1; i < n + 1; ++i) {
-            f *= i;
-            sum += 1.0 / f;
-        }
-        System.out.println(sum);  */
-
-        //float a = 1.1f, b = 1.3f, c;
-
-        //c = a / b;
-        //System.out.println(c);
-        //System.out.println(a);
-
-
-
-      //  double sum = 0;
-      //  int k, a = 2 ;
+       }
     }
 }

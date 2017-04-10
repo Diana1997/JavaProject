5ae32b75c10d27c779833a4c3447637be4f84dc6
ITSchool.user <ITSchool.user>
2017-04-10 16:32:47 +0400

    series

--- src/Test.java
+++ src/Test.java
@@ -4,57 +4,75 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  * Created by ITSchool.user on 04.04.2017.
  */
 public class Test {
-    public static void main(String[] args) {
-  /*  int n=40, count=0, k = 2;
-    while(n % k == 0){
-        count++;
-        k*=2;
-    }
-        System.out.println(count);
-    }*/
+    public static void main(String[] args){
 
-
-       int n = 7, k = 0, sum = 0;
-        while (true) {
-            if (sum > n) {
-                break;
-            }
-
-            sum += ++k;
+    //series 1-3
+        /*int[] arr = {1,2 ,3,4,5,6,7,8,9,10};
+        int sum = 0, p = 1;
+        for(int i=0; i <arr.length; ++i){
+            sum += arr[i];
+            p *=  arr[i];
         }
-        System.out.print(k);
-       /* if (sum == n) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
+        System.out.println(sum);
+        System.out.println(p);
+        System.out.println( (double) sum/2);
+        */
 
-        }*/
-
-
-   /*     int  p = 100, l = 2;
-        switch(l){
-            case 2:
-                p=   p / 1000000;
-                break;
-            case 3:
-                p = p / 1000;
-                break;
-            case 4:
-                p = p * 1000;
-                break;
-            case 5:
-                p = p * 100;
+       /* int n  = 5, a= 2, b = 5;
+        double arr[] = new double[n];
+        double sum = 0, p = 1;
+        for(int i = 0; i < n; ++i){
+            arr[i] =(b - a + 1) * Math.random() + a ;
+            System.out.println(arr[i]);
+            sum += arr[i];
+            p += arr[i];
         }
-        System.out.println(p);*/
+        System.out.println(sum);
+        System.out.println(p); */
 
-        /*int sum = 0;
-        for(int i = 0; i<21; i++){
-            if(i % 3 == 0){
-                continue;
-            }
-            sum += i;
+       //series5
+      /*  int n  = 5, a= 2, b = 5;
+        double arr[] = new double[n];
+        double sum = 0;
+        for(int i = 0; i < n; ++i) {
+            arr[i] = (b - a + 1) * Math.random() + a;
+            System.out.println((int) arr[i]);
+            sum += (int) arr[i];
+        }
+        System.out.println(sum);
+        */
+
+        //series6
+    /*    int n  = 5, a= 2, b = 5;
+        double arr[] = new double[n];
+        double sum = 0, p = 1;
+        for(int i = 0; i < n; ++i) {
+            arr[i] = (b - a + 1) * Math.random() + a;
+            System.out.println(arr[i] - (int) arr[i]);
+            p *= arr[i] - (int) arr[i];
         }
         System.out.println(sum);
 */
+        //series7
+       /* int n  = 5, a= 2, b = 10;
+        double arr[] = new double[n];
+        double sum = 0, p = 1;
+        for(int i = 0; i < n; ++i) {
+            arr[i] = (b - a + 1) * Math.random() + a;
+            double c  = Math.round(arr[i] - (int) arr[i]);
+            System.out.println(c);
+            p *= c;
+        }
+        System.out.println(p);
+
+        */
+
+        //series8
+        int n  = 6;
+        int  arr[] = new int[n];
+        for(int i=0; i<n; ++i){
+            arr[i] =  i * i;
+        }
+
     }
 }
